3e04943f2a2f1642e81e19cec940185605fc52f4
Arnold Mavhezha <arnoemavhezha@gmail.com>
2020-01-10 11:32:08 +0530

    My  commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+        //Eureka!!! I made a comment. "smile, smile"
         [Key]
         public int CartItemId { get; set; }
 
